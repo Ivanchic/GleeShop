2f8a449fb03a97866683b80f84e4e498295cf98c
Ivanchik <iradolgaya88@icloud.com>
2024-11-13 17:30:27 +0400

    added terms

--- gulpfile.js
+++ gulpfile.js
@@ -33,8 +33,9 @@ function scripts() {
   return src([
     "node_modules/jquery/dist/jquery.js",
     "node_modules/slick-carousel/slick/slick.js",
-     "app/js/main.js",
-    ])
+    "node_modules/fancybox/dist/js/jquery.fancybox.js",
+    "app/js/main.js",
+  ])
     .pipe(concat("main.min.js"))
     .pipe(uglify())
     .pipe(dest("app/js"))
